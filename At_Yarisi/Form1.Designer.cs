00ed6b18f6636d12a573fea2d0fb8c0c436246bf
hp <hp@DESKTOP-51PMUPL>
2022-08-05 20:24:32 +0400

    Add project files.

---
+++ At_Yarisi/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace At_Yarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.pBox_at1 = new System.Windows.Forms.PictureBox();
+            this.pBox_at2 = new System.Windows.Forms.PictureBox();
+            this.pBox_at3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_restart = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_at1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_at2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_at3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(0, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(800, 23);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(0, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(800, 23);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(0, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(800, 23);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(797, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 350);
+            this.label5.TabIndex = 4;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.Goldenrod;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_start.Location = new System.Drawing.Point(12, 375);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(112, 52);
+            this.btn_start.TabIndex = 5;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // pBox_at1
+            // 
+            this.pBox_at1.Image = ((System.Drawing.Image)(resources.GetObject("pBox_at1.Image")));
+            this.pBox_at1.Location = new System.Drawing.Point(3, 26);
+            this.pBox_at1.Name = "pBox_at1";
+            this.pBox_at1.Size = new System.Drawing.Size(121, 80);
+            this.pBox_at1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox_at1.TabIndex = 6;
+            this.pBox_at1.TabStop = false;
+            // 
+            // pBox_at2
+            // 
+            this.pBox_at2.Image = ((System.Drawing.Image)(resources.GetObject("pBox_at2.Image")));
+            this.pBox_at2.Location = new System.Drawing.Point(3, 135);
+            this.pBox_at2.Name = "pBox_at2";
+            this.pBox_at2.Size = new System.Drawing.Size(121, 80);
+            this.pBox_at2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox_at2.TabIndex = 7;
+            this.pBox_at2.TabStop = false;
+            // 
+            // pBox_at3
+            // 
+            this.pBox_at3.Image = ((System.Drawing.Image)(resources.GetObject("pBox_at3.Image")));
+            this.pBox_at3.Location = new System.Drawing.Point(3, 244);
+            this.pBox_at3.Name = "pBox_at3";
+            this.pBox_at3.Size = new System.Drawing.Size(121, 80);
+            this.pBox_at3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox_at3.TabIndex = 8;
+            this.pBox_at3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Sienna;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei Light", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(248, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(357, 52);
+            this.label6.TabIndex = 9;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Sienna;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei Light", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(611, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 52);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "0";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_restart
+            // 
+            this.btn_restart.BackColor = System.Drawing.Color.Goldenrod;
+            this.btn_restart.Enabled = false;
+            this.btn_restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_restart.Location = new System.Drawing.Point(130, 375);
+            this.btn_restart.Name = "btn_restart";
+            this.btn_restart.Size = new System.Drawing.Size(112, 52);
+            this.btn_restart.TabIndex = 11;
+            this.btn_restart.Text = "Restart";
+            this.btn_restart.UseVisualStyleBackColor = false;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Goldenrod;
+            this.btn_exit.Enabled = false;
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_exit.Location = new System.Drawing.Point(688, 375);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(112, 52);
+            this.btn_exit.TabIndex = 12;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_restart);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pBox_at3);
+            this.Controls.Add(this.pBox_at2);
+            this.Controls.Add(this.pBox_at1);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "At Yarisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_at1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_at2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_at3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.PictureBox pBox_at1;
+        private System.Windows.Forms.PictureBox pBox_at2;
+        private System.Windows.Forms.PictureBox pBox_at3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn_restart;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ At_Yarisi/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace At_Yarisi
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int at_1_soluzaqliq, at_2_soluzaqliq, at_3_soluzaqliq;
+        Random rand = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            at_1_soluzaqliq = pBox_at1.Left;
+            at_2_soluzaqliq = pBox_at2.Left;
+            at_3_soluzaqliq = pBox_at3.Left;
+
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            pBox_at1.Left = 0;
+            pBox_at2.Left = 0;
+            pBox_at3.Left = 0;
+            label6.Text = "";
+            label7.Text = "";
+            Application.Restart();
+
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int xal = Convert.ToInt32(label7.Text);
+            xal++;
+            label7.Text = xal.ToString();
+
+
+            int at_1_genislik = pBox_at1.Width;
+            int at_2_genislik = pBox_at2.Width;
+            int at_3_genislik = pBox_at3.Width;
+
+            int bitme_uzaqligi = label5.Left;
+            pBox_at1.Left += rand.Next(5, 10);
+            pBox_at2.Left += rand.Next(5, 10);
+            pBox_at3.Left += rand.Next(5, 10);
+
+            if (pBox_at1.Left > pBox_at2.Left +5 && pBox_at1.Left > pBox_at3.Left +5)
+            {
+                label6.Text = "Birinci at yarisda irelidedir";
+            }
+            if (pBox_at2.Left > pBox_at1.Left + 5 && pBox_at2.Left > pBox_at3.Left + 5)
+            {
+                label6.Text = "Ikinci at liderliyi ele kecirir ";
+            }
+            if (pBox_at3.Left > pBox_at1.Left + 5 && pBox_at3.Left > pBox_at2.Left + 5)
+            {
+                label6.Text = "Ucuncu at ireli kecir";
+            }
+            if (at_1_genislik+ pBox_at1.Left > bitme_uzaqligi)
+            {
+                timer1.Enabled=false;
+                label6.Text = "Birinci at qazandi";
+                btn_exit.Enabled=true;
+                btn_restart.Enabled = true;
+
+            }
+            if (at_2_genislik + pBox_at2.Left > bitme_uzaqligi)
+            {
+                timer1.Enabled = false;
+                label6.Text = "Ikinci at qazandi";
+                btn_exit.Enabled = true;
+                btn_restart.Enabled = true;
+
+
+            }
+            if (at_3_genislik + pBox_at3.Left > bitme_uzaqligi)
+            {
+                timer1.Enabled = false;
+                label6.Text = "Ucuncu at qazandi";
+                btn_exit.Enabled = true;
+                btn_restart.Enabled = true;
+
+
+            }
+
+        }
+
+    }
+}
